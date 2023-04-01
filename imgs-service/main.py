77e7922d0667917faa6413699e6cd24d7a985713
Wojciech Jasiński <woj.jasinski@gmail.com>
2023-04-01 22:43:36 +0200

    img generation script changes

--- imgs-service/main.py
+++ imgs-service/main.py
@@ -1,5 +1,6 @@
 import json
 import os
+import random
 
 from img_generation import generate_img
 from prompt_generation import generate_prompts
@@ -8,13 +9,12 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--topics", type=str, default="topics.txt")
-    parser.add_argument("--style", type=str, default="digital art")
     parser.add_argument("-n", type=int, default=4)
     return parser.parse_args()
 
 
 def generate_batch(topic, style, n):
-    cur_dir = f"imgs/{topic.replace(' ', '_')}"
+    cur_dir = f"new_imgs/{topic.replace(' ', '_')}-{style.replace(' ', '_')}"
     os.makedirs(cur_dir, exist_ok=True)
 
     prompts = generate_prompts(topic, style, n)
@@ -41,8 +41,14 @@ def generate_batch(topic, style, n):
 
 if __name__ == "__main__":
     args = parse_args()
+    styles = ["digital art", "abstract", "realistic", "cartoon", "anime", "sketch", "watercolor"]
 
     topics = open(args.topics).read().splitlines()
     for topic in topics:
         print(f"Generating batch for topic {topic}...")
-        generate_batch(topic, args.style, args.n)
+        try:
+            style = random.choice(styles)
+            generate_batch(topic, style, args.n)
+        except Exception as e:
+            print(f"Error generating batch for topic {topic}: {e}")
+            continue
