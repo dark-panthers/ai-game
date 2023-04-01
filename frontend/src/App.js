86e82f05df3d846f8fd7cf954e0160ba12526992
jagodek <mgodek@student.agh.edu.pl>
2023-04-01 14:00:49 +0200

    created start page for game

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,25 @@
-import './App.css';
+import './App.css'
+import GameStartPage from './GameStartPage';
+
+
+const game ={
+  title: "game name",
+  desc: "tutorial tutorial",
+  instruction_image:{
+    imgs:[]
+  },
+  instruction_text: ""
+}
+
+
 
 function App() {
   return (
     <>
-      <GameStartPage />
+      <GameStartPage game={game} />
     </>
   );
 }
 
-function GameStartPage(){
-  return (
-    <div className="gameStartPage">
-
-    </div>
-  )
-}
 
 export default App;
