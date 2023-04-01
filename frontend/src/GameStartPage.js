cd3ccf8d211b67c85190114f3f70df5df9a2d550
jagodek <mgodek@student.agh.edu.pl>
2023-04-01 15:18:20 +0200

    Tutorial pop up template is done

--- frontend/src/GameStartPage.js
+++ frontend/src/GameStartPage.js
@@ -1,12 +1,27 @@
 import "./GameStartPage.css";
+import {useState} from "react"
+import Tutorial from "./Tutorial";
 
 export default function GameStartPage({ game }) {
   console.log(game);
+
+  const [tutorialVisibility, setTutorialVisiility] = useState(false);
+
+  function toggleTutorialVisibility(){
+    if(tutorialVisibility === false){
+      setTutorialVisiility(true);
+    }
+    else{
+      setTutorialVisiility(false);
+    }
+    console.log(tutorialVisibility);
+  }
 
   return (
     <div className="game-start-page">
       <div className="title-box">
         <h1 className="title">{game.title}</h1>
+        {tutorialVisibility ? <Tutorial onclick={toggleTutorialVisibility} /> : ""}
       </div>
       <div className="start-box">
         <button>
@@ -17,11 +32,11 @@ export default function GameStartPage({ game }) {
         <img src={process.env.PUBLIC_URL + "/icons/gear.svg"} alt="ooops"></img>
       </div>
       <div className="top-right">
-        <img
+        <img 
           src={process.env.PUBLIC_URL + "/icons/share.svg"}
           alt="ooops"
         ></img>
-        <img
+        <img onClick={toggleTutorialVisibility}
           src={process.env.PUBLIC_URL + "/icons/patch-question.svg"}
           alt="ooops"
         ></img>
